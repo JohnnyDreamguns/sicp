4a654959fab7a6737517488d45327110760e9410
John Hodgson <mrjohnhodgson@gmail.com>
2020-06-05 23:07:21 +0100

    1.5.scm

---
+++ 1.5.scm
@@ -0,0 +1,3 @@
+; With applicative-order the compound procedure will be evaluated and result
+; in infinite recursion. With normal order the procedure will return false
+; and the procedure will never be executed, 0 will be returned
